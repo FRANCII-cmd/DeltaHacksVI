7a8a14ef754bb9d77afdd6c942692101e34c5f93
Michael Francis <ian.michael.francis@ibm.com>
2020-01-25 17:40:29 -0500

    Co-authored-by: OverlordActual11 <zayaankhan@rocketmail.com> Co-authored-by: adilferoz@outlook.com <adilferoz@outlook.com>

---
+++ TInterpreter.py
@@ -0,0 +1,7 @@
+# Tedxt Based Interpreter base off of the Natural Language Toolkit 
+#NLTK ONLY Please 
+
+# Test Code for the 
+def Fileopenfunctions(Testfile):                                 #This for file reading 
+    file = open(Testfile,r)
+    print(file)
--- Workspace1.py
+++
@@ -1,5 +0,0 @@
-from textblo
-
-text = ("Football is a good game. It has many health benefit")
-text_blob_object = TextBlob(text)
-print(text_blob_object.words.pluralize())
